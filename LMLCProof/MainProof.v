501acf38a61900423a30f7d28b2c3b0794d99e66
zach <ZachMGH>
2024-09-26 18:59:35 +0200

    Added further lemmas and advanced in proofs

--- LMLCProof/MainProof.v
+++ LMLCProof/MainProof.v
@@ -27,6 +27,9 @@ Proof. unfold reflexive. intro x. unfold beta_star.
         ++ reflexivity.
 Qed.
 
+Lemma beta_red_is_transitive : transitive lambda_term (beta_star).
+Proof. Admitted.
+
 Lemma beta_subset_beta_star : forall (M N : lambda_term), M ->b N -> M ->b* N.
 Proof. intros M N H. unfold beta_star. unfold refl_trans_closure. exists [M;N].
        simpl. split.
@@ -58,9 +61,45 @@ Lemma beta_star_contextual_appl'r :
   forall (M N N': lambda_term), N ->b* N' -> Lappl M N ->b* Lappl M N'.
 Admitted.
 
+(* MAIN PROOF *)
+
+Lemma lmlc_substitution : forall (M N : ml_term) (x : var),
+                          lmlc (ml_substitution M N x) = substitution (lmlc M) (lmlc N) x.
+Proof. induction M as [ x | M1 IHappl1 M2 IHappl2 | x M' IHfunbody| f x M' IHfixfunbody
+                      | M1 IHplus1 M2 IHplus2 | M1 IHminus1 M2 IHminus2 | M1 IHtimes1 M2 IHtimes2 | n
+                      | M' IHgtz
+                      | | | C IHifc T IHift E IHife
+                      | HD IHconshd TL IHconsnil| |LST IHfoldlst OP IHfoldop INIT IHfoldinit
+                      | P1 IHpair1 P2 IHpair2 | P IHfst | P IHsnd ].
+  - intros *. simpl. destruct (eqb x0 x).
+    + reflexivity.
+    + reflexivity.
+  -
+Admitted.
+
+(**
+If you want to induct :
+[ y | L1 IHappl1' L2 IHappl2' | y L IHfunbody'| g y L IHfixfunbody'
+| L1 IHplus1' L2 IHplus2' | L1 IHminus1' L2 IHminus2' | L1 IHtimes1' L2 IHtimes2' | m
+| L IHgtz'
+| | | C' IHifc' T' IHift' E' IHife'
+| HD' IHconshd' TL' IHconsnil' | | LST' IHfoldlst' OP' IHfoldop' INIT' IHfoldinit'
+| P1' IHpair1' P2' IHpair2' | P' IHfst' | P' IHsnd' ]
+
+
+If you want to destruct :
+[ y | L1 L2 | y L | g y L
+| L1 L2 | L1 L2 | L1 L2 | m
+| L
+| | | C' T' E'
+| HD' TL' | | LST' OP' INIT'
+| P1' P2' | P' | P' ]
+
+*)
+
 Theorem lmlc_is_correct : forall (M N : ml_term), M ->ml N -> (lmlc M) ->b* (lmlc N).
-Proof. induction M as [ x | M1 IHappl1 M2 IHappl2| x M' IHfunbody| f x M' IHfixfunbody
-                      | M1 IHplus1 M2 IHplus2| M1 IHminus1 M2 IHminus2 | M1 IHtimes1 M2 IHtimes2 | n
+Proof. induction M as [ x | M1 IHappl1 M2 IHappl2 | x M' IHfunbody| f x M' IHfixfunbody
+                      | M1 IHplus1 M2 IHplus2 | M1 IHminus1 M2 IHminus2 | M1 IHtimes1 M2 IHtimes2 | n
                       | M' IHgtz
                       | | | C IHifc T IHift E IHife
                       | HD IHconshd TL IHconsnil| |LST IHfoldlst OP IHfoldop INIT IHfoldinit
@@ -68,15 +107,34 @@ Proof. induction M as [ x | M1 IHappl1 M2 IHappl2| x M' IHfunbody| f x M' IHfixf
 (* M = x *)
   - intros N H. simpl in H. exfalso. apply H.
 (* M = (M1)M2 *)
-  - intros N H. simpl. simpl in H. destruct M1 eqn:eqM1.
-    + simpl. simpl in H. destruct N eqn:eqN. all : try (exfalso; apply H).
+  - intros N H. simpl. simpl in H. destruct M1 as 
+                                                  [ y | L1 L2 | y L | g y L
+                                                  | L1 L2 | L1 L2 | L1 L2 | m
+                                                  | L
+                                                  | | | C' T' E'
+                                                  | HD' TL' | | LST' OP' INIT'
+                                                  | P1' P2' | P' | P' ].
+    (* M1 = y *)
+    + simpl. simpl in H. destruct N. all : try (exfalso; apply H).
       * simpl. destruct H.
         ++ exfalso. apply H.
-        ++ destruct H as [H1 H2]. rewrite <- H1. simpl. apply IHM2 in H2.
+        ++ destruct H as [H1 H2]. rewrite <- H1. simpl. apply IHappl2 in H2.
            apply beta_star_contextual_appl'r. apply H2.
-    + destruct N eqn:eqN. all : try (exfalso; apply H).
-      * simpl. destruct H.
-        ++
+    (* M1 = (L1)L2 *)
+    + destruct N. all : try (exfalso ; apply H).
+      * destruct H.
+        ++ destruct H. apply IHappl1 in H. simpl. rewrite H0.
+           apply beta_star_contextual_appl'l. simpl in H. apply H.
+        ++ destruct H. rewrite H. simpl. apply beta_star_contextual_appl'r. apply IHappl2. apply H0.
+    (* M1 = fun y -> L *)    
+    + destruct N as 
+                  [ z | T1 T2 | z T | h z T
+                  | T1 T2 | T1 T2 | T1 T2 | m
+                  | T
+                  | | | C'' T'' E''
+                  | HD'' TL'' | | LST'' OP'' INIT''
+                  | P1'' P2'' | P'' | P'' ].
+      * simpl. 
 (* M = fun x -> M' *)
   -
 (* M = fixfun f x -> M' *)
